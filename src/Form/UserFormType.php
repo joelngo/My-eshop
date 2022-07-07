9cbe98e760e6617a3ee469414a683698d44508f4
joelngo <108331383+joelngo@users.noreply.github.com>
2022-07-07 05:56:35 +0200

    configuration et user

--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -12,13 +12,61 @@ class UserFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('genre')
-        ;
-    }
+            ->add('email',EmailType::class, [
+                 'label' =>'E-mail',
+                 'constraints'=>[
+                    new Email([
+                        'message' =>"votre email n'est pas au bon format :mail@exemple.fr"
+                    ]),
+                    new NotBlank([
+                        'message' => 'Ce champ ne peut pas etre vide'
+                    ])
+                    new length([
+                        'min' =>8,
+                        'max' =>255,
+                        'minMessage'=>"votre email doit comporte{{link}} caracteres minimum.",
+                        'minMessage'=>"votre email doit comporte{{link}} caracteres minimum.",
+                    ]),
+                 ],  
+                 'help' => "* min caracteres : 8
+                            * max carcateres :255
+                            *au moins 1 caractere special,
+                            *au moins 1 majuscule,
+                            *au moins 1 minuscule,
+                            *au moins 1 chiffre"
+             ])
+            ->add('password', PasswordType::class,[
+                 'label' =>'Mot de passe'
+             ])
+
+
+            ->add('firstname', TextType::class,[
+                 'label' =>'Prenom'
+             ])
+
+
+            ->add('lastname', TextType::class, [
+                 'label' =>'Nom'
+              ])
+
+
+            ->add('genre', ChoiceType::class,[
+                 'label' =>'sexe'
+                 'expanded' => true
+                 'choices' =>[
+                     "Homme" => 'homme'
+                     "Femme" => 'femme'
+
+                 ],
+            ;
+              
+       
+              
+                ]),
+   
+            }
+
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
